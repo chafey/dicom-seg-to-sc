401a89481492ce701268f466aab04023d6639679
Chris Hafey <cehafey@amazon.com>
2024-01-17 15:41:01 -0600

    added build script that deletes old generated dicom

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+rm dicomout/*
+npm start
--- getParameters.js
+++ getParameters.js
@@ -2,7 +2,6 @@ const getParameters = () => {
   if (process.argv.length != 4) {
     throw "Not enough command line arguments - expected input file and output directory";
   }
-  console.log(process.argv);
   return {
     inputFile: process.argv[2],
     outputDirectory: process.argv[3],
